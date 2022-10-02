3be911f2d500d6a7e7eeecf0618b1e3bcc72efcd
Matthew Champagne <75323501+Allotec@users.noreply.github.com>
2022-10-02 02:05:48 -0400

    Delete NeuralNetLibrary/build directory

--- NeuralNetLibrary/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Matthew Champagne/OneDrive/Fall 2022/ESE 440/NeuralNetLibrary
-# Build directory: C:/Users/Matthew Champagne/OneDrive/Fall 2022/ESE 440/NeuralNetLibrary/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
