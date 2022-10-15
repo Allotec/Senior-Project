e5490bab889d17a6735d664076c455f472bf7e85
FifiSaysHi <fiona.tong2001@gmail.com>
2022-10-15 00:04:51 -0400

    Flatten and Dense Functions Update
    completed dense and flatten functions with test cases

--- NeuralNetLibrary/main.cpp
+++ NeuralNetLibrary/main.cpp
@@ -11,7 +11,36 @@ int main(){
     m(0, 1) = -1;
     m(1, 1) = m(1, 0) + m(0, 1);
 
-    std::cout << m.size() << std::endl;
+    //Array:
+    //3  -1
+    //2.5 1.5
 
+    std::cout << "Size of Initial Matrix m: " << m.size() << std::endl;
+    std::cout << m << std::endl;
+    std::cout << std::endl;
+
+
+    //Testing Flattening
+    Eigen::MatrixXf output1 = flatten(m);
+    std::cout << "Flattened Result: \n" << output1 << std::endl;
+
+    //Testing Dense Layers 
+    //function assumes that input matrix has been flattened 
+    //in this case input is output1 from  previous test
+    Eigen::MatrixXf weights(4, 6), bias(1, 6); //input is 1x4, weight is 4x6 so product is 1x6 
+    weights << 1, 2, 3, 4, 5, 6,
+        2, 3, 4, 5, 6, 7,
+        3, 4, 5, 6, 7, 8,
+        4, 5, 6, 7, 8, 9;
+    bias << 1, 1, 1, -50, 1, 1;
+    std::cout << "\nWeights Matrix: \n" << weights << std::endl;
+    std::cout << "\nBias Matrix: \n" << bias << std::endl;
+    Eigen::MatrixXf output2 = dense(output1, weights, bias, "linear");
+    std::cout << "\nDense Layer Result: \n" << output2 << std::endl;
+    Eigen::MatrixXf output3 = dense(output1, weights, bias, "relu");
+    std::cout << "\nDense Layer Result: \n" << output3 << std::endl;
+
+    Eigen::MatrixXf output4 = dense(output1, weights, bias, "softmax");
+    std::cout << "\nDense Layer Result: \n" << output4 << std::endl;
     return (0);
 }
--- NeuralNetLibrary/networkOps.cpp
+++ NeuralNetLibrary/networkOps.cpp
@@ -1,4 +1,11 @@
 #include "networkOps.hpp"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <Eigen/Dense>
+#include <unsupported/Eigen/MatrixFunctions>
+
 
 //Convolution operation
 Eigen::MatrixXf convolution(
@@ -37,20 +44,65 @@ Eigen::MatrixXf dense(
     Eigen::MatrixXf bias, //Bias matrix
     std::string activation //Activation function (relu, sigmoid, tanh, softmax, linear, ...)
     ){
-        //Retun empty matrix
-        Eigen::MatrixXf output;
+        
+        Eigen::MatrixXf hidden, output;
+        hidden = (input * weights) + bias;// H = f(I*W + B)
 
-        //Return output
-        return output;
+        std::transform(activation.begin(), activation.end(), activation.begin(), ::toupper); //convert to all caps to ignore case
+        std::cout << "\nActivation: " << activation << std::endl;
+        if (activation.compare("SOFTMAX") == 0) {
+            //loop to loop thru all values in hidden
+            //den = e^input
+            Eigen::MatrixXf temp(1, hidden.cols()), numer(1, hidden.cols());
+            double num = 0;
+            for (int i = 0; i < hidden.cols(); i++) {//gets denominator 
+                temp(0, i) = exp(hidden(0, i));
+                std::cout << "iteration " << i << std::endl;
+                num += temp(0, i);
+                std::cout << "sum: " << num << std::endl;
+            }
+            
+            //output = temp;
+
+               
+            //loop through all values in hidden
+            //num = e^input
+            //output for that iteration = num/den
+            for (int i = 0; i < hidden.cols(); i++) {
+                numer(0, i) = exp(hidden(0, i));
+            }
+            
+            output = numer / num;
+        }
+        else if (activation.compare("RELU") == 0) {
+            
+            for (int i = 0; i < hidden.cols(); i++) {//assuming row vector
+                if (hidden(0, i) < 0) {//output = 0 if input is negative; else return input
+                    hidden(0, i) = 0;
+                }
+            }
+            output = hidden;
+        }
+        else {
+            std::cout << "Error: Invalid Activation Selected!" << std::endl;
+            output = hidden * 0;//output will return all 0s for all items in vector
+        }
+        
+        //output = hidden + bias;
+        return output;//Return output
     }
 
 //Flatten operation
 Eigen::MatrixXf flatten(
     Eigen::MatrixXf input //Input matrix
     ){
-        //Retun empty matrix
+        //std::cout << "Test Flatten: " << input.size() << std::endl;
         Eigen::MatrixXf output;
-
+        //output = m;
+        output = input.reshaped(1, input.size());
+        //output = input;
+        
+        //Eigen::Map<RowVectorXf> output(input.data(), 1, input.size());
         //Return output
         return output;
     }
